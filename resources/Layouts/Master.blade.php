1eddbced9fc7fdc307a5241d265db8dc412bccb4
arof44 <arafahnur57@gmail.com>
2020-11-21 16:50:18 +0700

    REST API

---
+++ resources/Layouts/Master.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>@yield('title')</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="{{ asset('css/blog-home.css') }}" rel="stylesheet">
+
+</head>
+
+
+<body>
+
+  @yield('header')
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            @can ('user-display')
+            <a class="nav-link" href="{{ url('./home') }}">Home
+              <span class="sr-only">(current)</span>
+            </a>
+            @endcan
+          </li>
+          <li class="nav-item">
+            @can ('user-display')
+            <a class="nav-link" href="{{ url('./about') }}">About
+            </a>
+            @endcan
+          </li>
+          <li class="nav-item dropdown">
+            @can ('manage-articles')
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Menu
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Manage</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="{{ url('./managearticle') }}">Kelola Aritkel</a>
+              <a class="dropdown-item" href="{{ url('./managearticle/add') }}">Add</a>
+              <a class="dropdown-item" href="{{ url('./managearticle') }}">Edit</a>
+              <a class="dropdown-item" href="{{ url('./managearticle') }}">Hapus</a>
+              <a class="dropdown-item" href="{{ url('./manageuser') }}">Kelola User</a>
+
+            </div>
+            @endcan
+          </li>
+          <li class="nav-item dropdown">
+            @guest
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @if (Route::has('register'))
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+            @endif
+            @else
+            <li class="nav-item dropdown">
+              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }} <span class="caret"></span>
+              </a>
+
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+              </form>
+            </div>
+          </li>
+          @endguest
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+@yield('content')
+
+<!-- /.container -->
+
+
+<!-- Footer -->
+<footer class="py-5 bg-dark">
+  <div class="container">
+    <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+  </div>
+  <!-- /.container -->
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+</body>
+
+</html>
